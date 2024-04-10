e19883d88eb66e4ee818e918f17bb3fa178e1e90
senaerdem <senanurerdem07@gmail.com>
2024-04-10 23:52:13 +0300

    added ListViewController for displaying travel list
    A simple view was created on the TableView to list the travels. Additionally, an 'Add' button was included, which transitions to the relevant screen for adding a new travel upon clicking. Currently, for demonstration purposes, 10 test travels are listed.

---
+++ TravelBook/ListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ListViewController.swift
+//  TravelBook
+//
+//  Created by Sena Nur Erdem on 10.04.2024.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    @objc func addButtonClicked() {
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+}
+
