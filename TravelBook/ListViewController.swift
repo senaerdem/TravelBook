33e1b4c3115ca41d4c51141616a929f335236315
senaerdem <senanurerdem07@gmail.com>
2024-04-11 19:47:42 +0300

    added CoreData integration for fetching travel data
    The titleArray and idArray were added to store the titles and IDs of the travels retrieved from CoreData. The getData() function was implemented to fetch data from CoreData and populate the arrays accordingly. The tableView's numberOfRowsInSection now returns the count of titleArray to dynamically display the correct number of rows based on the fetched data. Also, cellForRowAt indexPath was modified to display the titles fetched from CoreData instead of static 'test' texts.

--- TravelBook/ListViewController.swift
+++ TravelBook/ListViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import CoreData
 
 class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var titleArray = [String]()
+    var idArray = [UUID]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,6 +21,40 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        getData()
+    }
+    
+    func getData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                
+                self.titleArray.removeAll(keepingCapacity: false)
+                self.idArray.removeAll(keepingCapacity: false)
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if let title = result.value(forKey: "title") as? String {
+                        self.titleArray.append(title)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                }
+                tableView.reloadData()
+            }
+        }
+        catch {
+            print("error")
+        }
     }
     
     @objc func addButtonClicked() {
@@ -25,12 +62,12 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titleArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = titleArray[indexPath.row]
         return cell
     }
     
